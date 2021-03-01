a4d4614325d64fa5d98b840fbe572fc1c7763d42
inkfarer <inkfarer@inkfarer.com>
2021-03-02 00:21:35 +0200

    Add background to break screen

---
+++ graphics/scripts/break/backgroundAnimation.js
@@ -0,0 +1,14 @@
+wavify(document.getElementById('wave-path'), {
+    height: -500,
+    bones: 3,
+    amplitude: 75,
+    color: 'var(--background-1-dark)',
+    speed: 0.10
+});
+
+const coralTileScrollTl = gsap.timeline({repeat: -1});
+
+coralTileScrollTl.to('div.coral-wrapper', {
+    duration: 15,
+    ease: Power0.easeNone,
+    backgroundPosition: '400px 1300px'});
